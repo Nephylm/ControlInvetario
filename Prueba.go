a2c7bc0e7b04c80f00a5329b9462bb2154b8a0c1
Jan Leví Hernández Flores <crownclown144@gmail.com>
2020-09-04 16:44:13 -0500

    correccion de errores

--- Prueba.go
+++ Prueba.go
@@ -101,8 +101,7 @@ func UpdateMembershipEndpoint(w http.ResponseWriter, req *http.Request){
 		json.NewEncoder(w).Encode(data)
 }
 
-func DeleteMembershipEndpoint(w http.ResponseWriter, req *http.Request) {
-	enableCors(&w)
+func DeleteMembershipEndpoint(w http.ResponseWriter, req *http.Request){
 	params := mux.Vars(req)
 	abrirConexionDB()
 	eliminarDatosBD(strconv.ParseInt(params["id"],10,64) )
@@ -111,12 +110,16 @@ func DeleteMembershipEndpoint(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(data)
 }
 func Upload(w http.ResponseWriter, req *http.Request){
+	req.ParseMultipartForm(32 << 20)
+	Archivo, _, _ := req.FormFile("file")
+	//fmt.Println(nombre)
+	//fmt.Println(r)
 	if (*req).Method == "OPTIONS" {
 		return
 	}
 	if (*req).Method=="POST"{
 		//var monitor Monitor
-		r:=csv.NewReader(req.Body)
+		r:=csv.NewReader(Archivo)
 		r.Comma=','
 		r.FieldsPerRecord = -1
 		if err != nil{
@@ -126,42 +129,23 @@ func Upload(w http.ResponseWriter, req *http.Request){
 
 		fmt.Println(campos)
 		for{
-			i:=0
 			mon, err:=r.Read()
 			if err == io.EOF{
 				break
 			}
 			item.Producto=make(map[string]string)
-			for _,c :=range mon{
-
+			for i,c :=range mon{
 				item.Producto[campos[i]]=c
-				i++
-
 			}
-			items=append(items,item)
-
-			/*
-			var m Monitor
-			m.Warehouse=mon[0]
-			m.Class=mon[1]
-			m.AssetSSE, _ =strconv.Atoi(mon[2])
-			m.Serial =mon[3]
-			m.Manuf=mon[4]
-			m.Model =mon[5]
-			m.ModelN =mon[6]
-			m.Cond =mon[7]
-			m.ScreenSize,_ =strconv.Atoi(mon[8])
-			m.NotesComments =mon[9]
-			m.NoSKU =mon[10]
-			m.Price,_=strconv.Atoi(mon[11])
-			monitores = append(monitores,m)*/
+			if item.Producto["Asset/SSE #"]!=""{
+				items=append(items,item)
+			}
 
 		}
-		//pantallas.Data=monitores
+
 		Lista.Data=items
 		json.NewEncoder(w).Encode(Lista)
 		fmt.Println(Lista)
-		//fmt.Println(monitores)
 	}
 }
 func PruebaMap(w http.ResponseWriter, req *http.Request){
@@ -200,14 +184,6 @@ func ReadExel(){
 		}
 		fmt.Println()
 	}*/
-}
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-}
-func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 func Iniciar() {
 
